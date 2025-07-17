55dff57ca3d766133ee687f25a001aa168ea2b2f
KKys <oops9798@gmail.com>
2025-07-17 18:04:54 +0900

    refactor : createVNode 수정

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,22 +1,11 @@
 export function createVNode(type, props, ...children) {
-  // 자식 배열을 평탄화하는 함수
-  const flattenChildren = (children) => {
-    return children.reduce((acc, child) => {
-      if (Array.isArray(child)) {
-        acc.push(...flattenChildren(child));
-      } else if (child !== null && child !== undefined && child !== true && child !== false) {
-        acc.push(child);
-      }
-      return acc;
-    }, []);
-  };
-
-  // 자식들을 평탄화하고 필터링
-  const normalizedChildren = flattenChildren(children);
-
   return {
     type,
     props,
-    children: normalizedChildren,
+    children: flattenChildren(children),
   };
+}
+
+function flattenChildren(children) {
+  return children.flat(Infinity).filter((value) => value === 0 || Boolean(value));
 }
